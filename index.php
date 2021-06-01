d694b5675c9b1fa0cc441ced2d74be7d88ed3c8f
TenguMask <35844109+TenguMask@users.noreply.github.com>
2021-06-01 15:51:14 +0300

    flex

--- index.php
+++ index.php
@@ -4,15 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width = device-width, initial-scale = 1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="stylesheet" href="style.css">
     <title> Keat </title>
 </head>
 
 <body>
-    <?php
-
-        $link = mysqli_connect('localhost',"root","","diploma");
-        $query = mysqli_query($link, "
+    <?php $link=mysqli_connect( 'localhost', "root", "", "diploma");
+    $query=mysqli_query($link, "
 SELECT items.id,
        items.name,
        items.price,
@@ -23,15 +21,14 @@ SELECT items.id,
 FROM   items
        INNER JOIN pics
                ON items.id = pics.items_id");
+    foreach ($query as $a){ echo $a[ 'id'] . " " . $a[ 'name'] . " " . $a[ 'price'] . " " . $a[ 'price'] . " " . $a[ 'pic1']. "<br>"; } ?>
 
+    <div class="flex-container">
+        <div class="flex-item"> </div>
+         <div class="flex-item"> </div>
+         <div class="flex-item"> </div>
+    </div>
 
-
-    foreach ($query as $a){
-        echo $a['id']     . " "   . $a['name']   . " " .
-             $a['price']  . " "   . $a['price']  . " " .
-             $a['pic1']. "<br>";
-    }
-?>
-</body>
+    </body>
 
 </html>
