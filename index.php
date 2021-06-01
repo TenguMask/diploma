a7e1668195e3520c1b892f99c029a3de218747fd
TenguMask <35844109+TenguMask@users.noreply.github.com>
2021-06-01 15:43:24 +0300

    db query

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title> Keat </title>
+</head>
+
+<body>
+    <?php
+
+        $link = mysqli_connect('localhost',"root","","diploma");
+        $query = mysqli_query($link, "
+SELECT items.id,
+       items.name,
+       items.price,
+       items.description,
+       pics.pic1,
+       pics.pic2,
+       pics.pic3
+FROM   items
+       INNER JOIN pics
+               ON items.id = pics.items_id");
+
+
+
+    foreach ($query as $a){
+        echo $a['id']     . " "   . $a['name']   . " " .
+             $a['price']  . " "   . $a['price']  . " " .
+             $a['pic1']. "<br>";
+    }
+?>
+</body>
+
+</html>
